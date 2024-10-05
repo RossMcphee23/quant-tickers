44d6a34f9e6743f3cc295cac734e1fe26318281b
Ross Mcphee <rossmcphee82@gmail.com>
2024-10-06 00:04:38 +0100

    getting info from yfinance

--- comptetiors_ticker.py
+++ comptetiors_ticker.py
@@ -2,3 +2,11 @@ import yfinance as yf
 import pandas as pd
 from bs4 import BeautifulSoup
 import requests
+
+def get_competitors(ticker):
+    # Want to find competitors for the given stock ticker using yfinance
+    url = f"https://finance.yahoo.com/quote/{ticker}/profile?p={ticker}"
+    response = requests.get(url)
+    s = BeautifulSoup(response.text, 'html.parser')
+
+    
